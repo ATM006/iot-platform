438f0083ca4a6f4104ed4c9889fac2a7faa3441e
ATM006 <18829897162@163.com>
2018-04-18 22:08:43 +0800

    站点数据格式标准化

--- core/src/sites.py
+++ core/src/sites.py
@@ -3,21 +3,44 @@
 from flask import Flask
 from flask import request,jsonify
 from flask_pymongo import PyMongo
-import json
-import uuid
+import json,uuid,datetime
+
+exp = '{\
+"createdDate": "",\
+"createdBy": "",\
+"token":"",\
+"name":"",\
+"description":"",\
+"metadata": {}\
+}'
 
 
 def site_get(mongo):
-	return "GET sites"
+	sites = mongo.db.sites
+	s = sites.find()
+	out = []
+	for item in s:
+		item.pop("_id")
+		print(item)
+		out.append(item)
+	
+	return jsonify({'result':out})
 
 
 def site_post(mongo,data):
 	sites = mongo.db.sites
+	date = datetime.datetime.now()
+	ex = json.loads(exp)
 	token =data["token"]
 	if token == '':
-		data["token"] = uuid.uuid1()
-	#print(data)
-	sites.insert(data)
+		ex["createdDate"] = date.strftime("%Y-%m-%d %H:%M:%S")
+		ex["createdBy"] = "admin"
+		ex["token"] = str(uuid.uuid1())
+		ex["name"] = data["name"]
+		ex["description"] = data["description"]
+		ex["metadata"] = data["metadata"]
+	print(ex)
+	sites.insert(ex)
 	return "POST sites"
 
 def site_del(mongo):
